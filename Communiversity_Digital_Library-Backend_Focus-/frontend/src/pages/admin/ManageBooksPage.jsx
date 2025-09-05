4313f9b8e9c1eb162fa08e5f154c898e47060762
madscie <33217703+madscie@users.noreply.github.com>
2025-09-05 15:45:16 +0200

    Delete Communiversity_Digital_Library-Backend_Focus- directory
    change of root directory, joseph to push new files

--- Communiversity_Digital_Library-Backend_Focus-/frontend/src/pages/admin/ManageBooksPage.jsx
+++
@@ -1,9 +0,0 @@
-const ManageBooksPage = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold mb-6">Manage Books</h1>
-      <p>Here, admins will be able to view, edit, and delete books from the catalog.</p>
-    </div>
-  );
-};
-export default ManageBooksPage;
