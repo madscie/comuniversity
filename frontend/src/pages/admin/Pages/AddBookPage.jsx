cd501ae587af739a6487d0bb27102b72064cbc88
madscie <33217703+madscie@users.noreply.github.com>
2025-11-12 13:44:38 +0200

    Create AddBookPage.jsx

---
+++ frontend/src/pages/admin/Pages/AddBookPage.jsx
@@ -0,0 +1,11 @@
+// src/pages/admin/AddBookPage.jsx
+const AddBookPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Add a New Book</h1>
+      <p>This form will be used to manually enter new books into the system.</p>
+    </div>
+  );
+};
+
+export default AddBookPage;
